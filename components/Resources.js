48e5c227de38bb1cadfea2ab522406df473bbd71
unknown <david.odonohue@korddefence.com.au>
2021-02-23 11:14:25 +1100

    commit

--- components/Resources.js
+++ components/Resources.js
@@ -14,10 +14,12 @@ const Resources = Vue.component('resources', {
     <p>
     <br><br>
     <h3>For Clinicians</h3>
+    <p>
     <a href="https://www.psychologyboard.gov.au/Standards-and-Guidelines/Codes-Guidelines-Policies.aspx">AHPRA Guidelines and Policies</a><br>
     <a href="https://www.psychology.org.au/getmedia/d873e0db-7490-46de-bb57-c31bb1553025/18APS-Code-of-Ethics.pdf">APS Code of Ethics</a><br>
     <a href="https://www.psychology.org.au/getmedia/23c6a11b-2600-4e19-9a1d-6ff9c2f26fae/Evidence-based-psych-interventions.pdf">Evidence-based Psychological Interventions in the Treatment of Mental Disorders</a><br>
     <a href="https://www.psychology.org.au/Training-Events/Events-Calendar">APS Calendar of CPD events</a><br>
     <a href="https://www.psychology.org.au/inpsych/">APS InPsych magazine</a><br>
+    </p>
     </div>`
 });
