dccb440d98fbc332b02d5d78e0a31c2453d9c09f
unknown <david.odonohue@korddefence.com.au>
2021-02-24 19:20:29 +1100

    commit

---
+++ components/HiddenResources.js
@@ -0,0 +1,22 @@
+const HiddenResources = Vue.component('hidden-resources', {
+    template: `<div class="pageFrame">
+    <div class="heroImage" style="background-image: url('./assets/enjoy.jpg')">
+    <div class="heroText">
+    <h1 class="title">Resources</h1>
+    </div>
+    </div>
+    <div class="pageContent">
+    <span>
+    DSM-5: <a href="resources/dsm5.epub">EPUB</a> <a href="resources/dsm5.pdf">PDF</a>
+    <br><br>
+    <a href="resources/Character Strengths and Virtues.pdf">Character Strengths and Virtues</a>
+    <br><br>
+    <a href="resources/Clinical Handbook of Mindfulness.pdf">Clinical Handbook of Mindfulness</a>
+    <br><br>
+    <a href="resources/The Wiley Blackwell Handbook of Mindfulness.pdf">The Wiley Blackwell Handbook of Mindfulness</a>
+    <br><br>
+    <a href="resources/The Handbook of Person-Centred Psychotherapy and Counselling.pdf">The Handbook of Person-Centred Psychotherapy and Counselling</a>
+    </span>
+    </div>
+    </div>`
+});
