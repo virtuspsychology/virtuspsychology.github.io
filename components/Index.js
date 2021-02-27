10d0c0090dee324a245e0eb21b553633dd42a92e
unknown <david.odonohue@korddefence.com.au>
2021-02-27 15:06:33 +1100

    commit

--- components/Index.js
+++ components/Index.js
@@ -1,11 +1,23 @@
 const Index = Vue.component('index', {
+    methods: {
+        scrollToContent(){
+            var yValue = window.innerHeight - 48;
+            window.scrollTo({top:yValue, behavior: 'smooth'});
+        }
+    },
     template: `<div class="page-frame">
     <div class="lander">
+    <span></span>
+    <span>
     <h1 class="lander-text">In-person or telehealth therapy in Canberra</h1>
-    <p class="lander-text">Email me at <a href="mailto:virtuspsychology@gmail.com">virtuspsychology@gmail.com</a> or SMS me at
+    <p class="lander-text">Contact David via email at <a href="mailto:virtuspsychology@gmail.com">virtuspsychology@gmail.com</a> or via SMS at
     <a href="sms:0401609112">0401 609 112</a> to request an appointment or make an inquiry.</p>
+    </span>
+    <div @click="scrollToContent" class="bounce">
+    <svg xmlns="http://www.w3.org/2000/svg" height="60" viewBox="0 0 24 24" width="60"><path class="arrow" d="M7.4 8.6L12 13.2l4.6-4.6L18 10l-6 6-6-6 1.4-1.4z"/></svg>
     </div>
-    <div class="page-content">
+    </div>
+    <div class="page-content" id="content">
     David O'Donohue is a registered psychologist with a Master's degree in clinical psychology. 
     His primary focus will be to understand your individual and unique circumstances, and work with
     you to find an evidence-based therapeutic intervention that works for you.
@@ -15,7 +27,7 @@ const Index = Vue.component('index', {
     <br><br>
     David is currently accepting mental health care plan referred and self-referred clients experiencing symptoms related to anxiety and depression.
     <br><br>
-    To request an appointment or make an inquiry, please contact me via email at <a href="mailto:virtuspsychology@gmail.com">virtuspsychology@gmail.com</a> or
+    To request an appointment or make an inquiry, please contact David via email at <a href="mailto:virtuspsychology@gmail.com">virtuspsychology@gmail.com</a> or
     via SMS at <a href="sms:0401609112">0401 609 112</a>.
     </div>
     </div>`
