6ca18d058c6ce4b888b65f54336c379cd635d21b
unknown <david.odonohue@korddefence.com.au>
2021-03-01 11:47:38 +1100

    commit

--- components/FAQ.js
+++ components/FAQ.js
@@ -8,23 +8,51 @@ const FAQ = Vue.component('faq', {
     </div>
     <div class="page-content">
     <span class="question">What can I expect from therapy?</span>
-    <br>
+    <br><br>
+    Therapy will involve working collaboratively with your psychologist over the course of several 50 minute sessions. Your psychologist
+    is an expert on evidence-based treatments and interventions, and you are an expert on the factors in your own life, what you have tried before,
+    what has/hasn't worked, and your own goals an values. You may discuss the problems you are having in your life, learn and/or practice
+    various skills, experiment with different ways of thinking and/or behaving, and develop greater insight into yourself or others.
     <br><br>
     <span class="question">What will happen at the first session?</span>
     <br><br>
+    During the first session, your psychologist will ask you questions about your current goals and life factors. Your psychologist will ask about any
+    medication or medical conditions, suicidal thoughts, or psychotic symptoms - all clients are asked these questions, due to the critical importance they
+    may have on outcomes.
+    <br><br>
     <span class="question">How will progress be measured?</span>
+    <br><br>
+    At the start of therapy (either before, shortly after, or during your first session), you will be asked to complete a Depression, Anxiety, and Stress Scale.
+    This is a standardised, objective measure of the frequency and severity of symptoms of depression, anxiety, and general stress. After several sessions, you
+    will be asked to complete this scale again, and the results will be compared. You and your psychologist will reflect on the comparison, and identify the factors
+    which have contributed to any increase or decrease in the frequency or severity of symptoms.
     <br><br>
     <span class="question">Is all therapy the same?</span>
     <br><br>
+    It is not! Psychologists can differ greatly in their level of knowledge, experience, training, their personal beliefs, interpersonal skills, theoretical framework,
+    social/economic/cultural background, and many other ways which may have an impact on how engaging they are to you, and how helpful you find their approach.
+    Some clients may benefit more or less from certain psychologists than they do from others, and this can vary depending on the person. It is important to remember
+    this, as any negative or unhelpful experiences you or others may have had with therapy does not mean that all therapy will be of equal value to you.
+    <br><br>
     <span class="question">How much do you charge?</span>
     <br><br>
-    <span class="question">How long do sessions last?</span>
-    <br><br>
-    <span class="question">How many sessions will I need? How often?</span>
+    In line with the APS recommended schedule of fees for sessions with a private practice psychologist, a 50 minute session will cost $260. For clients who have not
+    used all of their mental health care plan sessions, this will equate to a $ gap fee after the $ rebate.
     <br><br>
     <span class="question">What is a mental health care plan? How does it work?</span>
     <br><br>
+    A mental health care plan is a plan that you create with your doctor, aimed at improving your mental health. This plan may involve sessions with a psychologist, in
+    which case Medicare will provide financial assistance towards these sessions. If you are unsure whether you have or need a mental health care plan, or whether you
+    can expect Medicare rebates from sessions with a psychologist, speak to your GP.
+    <br><br>
     <span class="question">Is my information private?</span>
+    <br><br>
+    Your psychologist will take notes on the things you discuss in your sessions. This information is encrypted with the same level of security that a bank uses, and
+    stored on a server. Only your psychologist will be able to access this information. Your psychologist will not share your information with anyone unless:<ul>
+    <li>You give consent for this information to be shared (e.g. to a GP or other health professional)</li>
+    <li>Your psychologist has reason to believe that you or someone else will be harmed if they do not disclose information. In this case, your psychologist will adopt
+    a harm minimisation approach, and take the course of action they deem justifiable</li></ul><br>
+    In all cases, your psychologist will value your privacy and dignity.
     <br><br>
     </span>
     </div>
