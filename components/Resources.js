41a8a4b45964e0a2592a542cb45d3dec8f001d8d
unknown <david.odonohue@korddefence.com.au>
2021-03-11 12:59:16 +1100

    commit

--- components/Resources.js
+++ components/Resources.js
@@ -8,7 +8,7 @@ const Resources = Vue.component('resources', {
     </div>
     <div class="page-content">
     <span>
-    <h3>For Clients</h3>
+    <h3>Clients - crisis options</h3>
     <a href="tel:131114">Lifeline</a><br>
     <a href="tel:1800551800">Kids Helpline</a><br>
     <a href="tel:1800629354">The Crisis Assessment and Treatment Team</a><br>
@@ -17,9 +17,11 @@ const Resources = Vue.component('resources', {
     <a href="tel:1300789978">Mensline</a><br>
     <a href="tel:1800650890">headspace</a><br> 
     <br><br>
-    <h3>For Clinicians</h3>
-    <a href="https://www.psychologyboard.gov.au/Standards-and-Guidelines/Codes-Guidelines-Policies.aspx">AHPRA Guidelines and Policies</a><br>
+    <h3>Clinicians - reference</h3>
+    <a href="https://www.ahpra.gov.au/About-Ahpra/What-We-Do/Legislation.aspx">AHPRA Legislation for Psychologists</a>
+    <a href="https://www.psychologyboard.gov.au/Standards-and-Guidelines/Codes-Guidelines-Policies.aspx">Psychology Board Guidelines and Policies</a><br>
     <a href="https://www.psychology.org.au/getmedia/d873e0db-7490-46de-bb57-c31bb1553025/18APS-Code-of-Ethics.pdf">APS Code of Ethics</a><br>
+    <a href="https://www.psychologyboard.gov.au/Standards-and-Guidelines/Registration-Standards.aspx">Psychology Board Registration Standards</a>
     <a href="https://www.psychology.org.au/getmedia/23c6a11b-2600-4e19-9a1d-6ff9c2f26fae/Evidence-based-psych-interventions.pdf">Evidence-based Psychological Interventions in the Treatment of Mental Disorders</a><br>
     <a href="https://www.psychology.org.au/Training-Events/Events-Calendar">APS Calendar of CPD events</a><br>
     <a href="https://www.psychology.org.au/inpsych/">APS InPsych magazine</a><br>
