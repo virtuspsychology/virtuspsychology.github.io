80ce2d953293dd79a982d3c8ba2d9218f0622e95
unknown <david.odonohue@korddefence.com.au>
2021-03-18 19:18:22 +1100

    commit

--- components/Psychometrics.js
+++ components/Psychometrics.js
@@ -9,7 +9,10 @@ const Psychometrics = Vue.component('psychometrics', {
     </div>
     <div class="page-content">
     <span>Please note: to preserve confidentiality, no data is recorded and autocomplete is explicitly disabled.
-    If you would like to keep a record of the data, please do so manually.</span>
+    If you would like to keep a record of the data, please either email, print, or manually record the data.<br><br>
+    <a href="psychometrics/DASS.html">Depression, Anxiety, and Stress Scale</a><br>
+    <a href="psychometrics/ORS.html">Outcome Rating Scale</a><br>
+    </span>
     </div>
     </div>
     </div>
