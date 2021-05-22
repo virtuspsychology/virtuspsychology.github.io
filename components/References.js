c6e5ca670c05b42e699120ddae69325ab9152c5a
David O'Donohue <virtuspsychology@gmail.com>
2021-05-22 19:36:26 +1000

    commit

---
+++ components/References.js
@@ -0,0 +1,9 @@
+<h3>Clinicians - reference</h3>
+<a href="https://www.psychology.org.au/getmedia/23c6a11b-2600-4e19-9a1d-6ff9c2f26fae/Evidence-based-psych-interventions.pdf">Evidence-based Psychological Interventions in the Treatment of Mental Disorders</a><br>
+<a href="https://www.psychology.org.au/getmedia/d873e0db-7490-46de-bb57-c31bb1553025/18APS-Code-of-Ethics.pdf">APS Code of Ethics</a><br>
+<a href="https://www.psychologyboard.gov.au/Standards-and-Guidelines/Codes-Guidelines-Policies.aspx">Psychology Board Guidelines and Policies</a><br>
+<a href="https://www.psychologyboard.gov.au/Standards-and-Guidelines/Registration-Standards.aspx">Psychology Board Registration Standards</a><br>
+<a href="https://www.psychology.org.au/Training-Events/Events-Calendar">APS Calendar of CPD events</a><br>
+<a href="https://www.psychology.org.au/inpsych/">APS InPsych magazine</a><br>
+<a href="https://www.ahpra.gov.au/About-Ahpra/What-We-Do/Legislation.aspx">AHPRA Legislation for Psychologists</a><br>
+<a href="resources/Mental Health Act 2015.pdf">Mental Health Act 2015</a><br>
