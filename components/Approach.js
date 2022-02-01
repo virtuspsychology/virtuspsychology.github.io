ba7240b6cbcdcf76bd9fc335d30e493b0e177647
David O'Donohue <virtuspsychology@gmail.com>
2022-02-01 20:32:08 +1100

    commit

--- components/Approach.js
+++ components/Approach.js
@@ -2,7 +2,7 @@ const Approach = Vue.component('approach', {
     template: `<div class="page-outer-frame">
     <div class="lander-filler"></div>
     <div class="page-inner-frame">
-    <div class="hero-image" style="background-image: url('./assets/eye.jpg')">
+    <div class="hero-image" style="background-image: url('./assets/eye.webp')">
     <div class="hero-text">
     <h1 class="title">Approach</h1>
     </div>
