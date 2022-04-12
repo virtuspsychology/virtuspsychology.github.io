b156975521b53f7ab2641359d5bd4db9f693fe65
David O'Donohue <virtuspsychology@gmail.com>
2022-04-12 20:05:04 +1000

    commit

--- components/Index.js
+++ components/Index.js
@@ -38,13 +38,45 @@ const Index = Vue.component('index', {
     <div class="page-content" id="content">
     Hello! My name is David O'Donohue, and I am a registered psychologist and clinical psychology registrar.
     <br><br>
-    My main area of interest is in trauma processing, and the resolution of internal conflict which results
-    from complex trauma. Sometimes it can feel like your mind is working against you. Whether it's the emergence of feelings which
-    are unwanted, thoughts which are unwanted, or impulses which are unwanted, a deeper understanding of the mind often reveals
-    that each of these unwanted processes have a positive intention for you. I aim to help clients understand the parts
-    of their mind, the intentions or functions of those parts, and eventually to bring each part into harmony. The goal is for the mind to work
-    with itself, rather than against itself.
-    I aspire to be reflective, curious, compassionate, and non-judgemental in my approach.
+    My main area of interest is complex trauma. Our minds generate a range of different thoughts, feelings, and impulses, all of
+    which have evolved because they are beneficial to our success as a species. Anxiety protects us against danger, sadness protects
+    us against wasting limited resources, and anger gives us the energy to improve an unfair situation. Sometimes, our mind learns
+    that a certain emotion is either unacceptable (because any effect of that emotion leads to drastically negative outcomes,
+    e.g. "I'll give you something to cry about" can lead to the idea that sadness is unacceptable), or vital (because the absence
+    of that emotion leads to drastically negative outcomes, e.g. "If I'm not anxious, I will miss the warning signs of danger").
+    When the mind is no longer free to let emotions come and go, and instead either needs an particular emotion to be present
+    all of the time or never at all, some unpleasant consequences can result.
+    <br><br>
+    If an emotion is deemed unacceptable, the experience of the emotion will be greatly intensified.
+    If you think that anxiety is ruining your life, you
+    might get anxious about being anxious. If you think that sadness is a sign of weakness, or a sign that you will never
+    enjoy anything, you might get sad about being sad. If you hate how angry you are, you might get angry when you feel the beginnings
+    of anger. Or, any combination of distressing emotion in response to a distressing emotion, leading to an intensely unpleasant
+    experience, confirming your mind's conscious or unconscious predictions about the emotion.
+    <br><br>
+    If an emotion is deemed vital, our mind can get into the habit of maintaining this emotion,
+    and over the years the original need for the emotion can be forgotten. If this emotion is unpleasant, we may develop some
+    resentment or resistance to the emotion, which can feed into the cycle that the above paragraph illustrates.
+    <br><br>
+    Many people enter therapy with the goal of eliminating the unpleasant emotion. However, it is often this goal which perpetuates
+    the distress, because it results in a much more intense experience of the emotion when it does arise. I envision the goal of
+    therapy to be a reduction in the intensity of the distress, by paradoxically being more accepting and tolerant of the emotion.
+    Becoming aware of pre-conscious mental processes and understanding their history and function can lead to a place where 
+    compassion and gratitude can be felt for the previously unwanted emotions. Imagery rescripting can help to set these processes
+    on a more adaptive track, through memory reconsolidation.
+    <br><br>
+    A quick note on judgement and shame: being judged or feeling shamed for the unwanted emotion, or a behaviour aimed at
+    eliminating the unwanted emotion, is a very common precursor to the kind of mental health stuggles identified above, and so it can
+    be normal for the mind to anticipate being judged by a therapist. In my mind, judgement arises under several possible conditions.
+    Firstly, feeling judgemental is a psychological defence against feeling inferior to someone. Feeling inferior can be distressing,
+    and so finding something to judge that person on can be soothing. Secondly, a person may not have enough information to understand
+    a course of action, and without understanding the reasons behind a course of action, a negative judgement can bridge the gap and
+    reduce uncertainty ("I don't understand why this person did that" compared to "this person did that because they are [negative
+    judgement]"). Thirdly, a person may have internalised a strong inner critic, and may display that critic to others as well.
+    Knowing these things about why judgement might arise can allow you to look at any judgemental thoughts that your mind generates
+    and think, "what are these thoughts protecting me from?". As such, I typically don't judge clients for anything at all. However,
+    this is not to say that I think that the fear of being judged is bad in some way - it is being generated from a place of
+    self-protection, and this place has likely been quite useful to you in the past.
     <br><br>
     I am currently accepting new clients. New and existing clients can make appointments either online
     through <router-link to="/Booking">the booking page</router-link>, or through the
---
+++ components/Model.js
@@ -0,0 +1,108 @@
+const Model = Vue.component('model', {
+    template: `<div class="page-outer-frame">
+    <div class="lander-filler"></div>
+    <div class="page-inner-frame">
+    <div class="hero-image" style="background-image: url('./assets/torii.webp')">
+    <div class="hero-text">
+    <h1 class="title">Model</h1>
+    </div>
+    </div>
+    <div class="page-content">
+    <h3>Summary</h3>
+    Cognitive behavioural therapy (CBT) is considered the gold standard of therapy, and traditional CBT is
+    recommended by many sources.
+    However, I think it is suboptimal. I believe CBT has some helpful components,
+    and some unhelpful components. This doesn't mean you can't benefit from CBT, or that any benefit you have recieved from
+    CBT isn't real or valuable. I believe that the best alternative to CBT, which contains the helpful components of
+    CBT without the unhelpful components, and a few additional helpful components, is Internal Family Systems (IFS) Therapy. The
+    components I try to bring to therapy are: increased awareness, distress tolerance, self-compassion, and memory reconsolidation.
+    Importantly, I do not think pathologising mental health, viewing mental processes as negative or unhelpful, or the
+    practice of challenging cognitions is helpful in most situations.
+    <br><br>
+    <h3>What is CBT?</h3>
+    Traditional CBT is based on the idea that between events and emotions, we have automatic interpretations, and that
+    our emotional response is based on these interpretations, rather than the event itself. CBT generally involves uncovering
+    these automatic interpretations, identifying biases in them, and challenging any biases, either with logic (the cognitive component),
+    or by behaving in different ways to gather more information (the behavioural component).
+    <br><br>
+    <h3>What does CBT get right?</h3>
+    A large component of CBT is about increasing awareness of automatic thoughts, realising that interpretations are being made,
+    and behaving in different ways such that the predictions your mind makes can be updated through experience.
+    All of these components are really helpful, and should be part of any therapy program.
+    <br><br>
+    <h3>What does CBT get wrong?</h3>
+    Something which is very common in people who are struggling with mental health is the idea that they are broken or deficient
+    in some way. The fact that CBT explains mental health issues as a result of cognitive biases which need to be corrected can sometimes
+    feel like a confirmation of this idea. In reality, we would not be able to function without
+    cognitive biases. In fact, cognitive biases are intentionally part of artificial intelligence systems, where they are called heuristics.
+    The reason for this is that without heuristics, or cognitive biases, our minds would be overwhelmed with the amount of information
+    which needs to be processed. Modern CBT (decribed in the Unified Protocol for Transdiagnostic Treatment of Emotional Disorders)
+    shifts emphasis from challenging distorted cognitions to something called cognitive flexibility.
+    Cognitive flexibility is the ability to recognise when an interpretation is being made, as it is happening,
+    and to generate other possible interpretations. Along with other changes, modern CBT appears to more closely
+    resemble Acceptance and Committment Therapy (ACT) than traditional CBT.
+    <br><br>
+    Additionally, while I think that CBT model of emotions is valid, I don't think it's the whole picture. This can be seen in the
+    case of phobias, in which a person feels intense anxiety in response to something which they don't necessarily logically believe
+    is dangerous. A common complaint with traditional CBT is that people can change the way they are thinking, but this doesn't always
+    change the way they are feeling. CBT attempts to address this by claiming that the belief is still there, it is just on a deeper
+    level, and it can be altered through behavioural experiments. However, Occam's razor would suggest that the emotion is a result
+    of classical conditioning, and any verbalised core belief would just be an interpretation of the feeling, rather than the cause
+    of the feeling.
+    <br><br>
+    <h3>What is IFS?</h3>
+    The IFS model talks about parts of the mind - as in, there is a part of me that wants to do this thing, and a part which is
+    holding me back. Each part is considered to have a positive intention for you, and wants you to do something which either
+    is protective, or has been protective in the past. Given that our environment is often changing, and each possible action
+    has pros and cons, internal conflict is a very common occurrence and is an explicit part of the IFS model.
+    As such, instead of trying to cognitively challenge and beat "negative"
+    thoughts, feelings, or impulses into logical submission, we can observe them, understand them, recognise their positive
+    intention for us, and feel gratitude and compassion for them. This has two helpful effects: firstly, it decreases
+    activity in a region of our brain called the default mode network, which has the subjective effect of feeling like these
+    thoughts, feelings, or impulses have less of a grip on us; secondly, by showing gratitude and compassion to these parts of
+    ourselves, we are much more able to tolerate their existence.
+    <br><br>
+    <h3>Which components do CBT and IFS share?</h3>
+    IFS, like CBT, assists in increasing awareness, as there is a big focus on being curious about our parts, and obseving them
+    experientially in the present moment. Again, like CBT, IFS encourages people to recognise that an interpretation has been
+    made, by viewing the thoughts, feelings, and impulses as being generated by a part, rather than as an objective truth.
+    Finally, IFS assists in behaviour change by giving yourself some distance from your impulses, but goes further than traditional
+    CBT by using imagery for the purpose of memory reconsolidation.
+    <br><br>
+    <h3>What other helpful components does IFS have?</h3>
+    In viewing our parts as having positive intentions for us, and understanding them rather than defeating them, IFS is very
+    non-pathologising/non-shaming/non-judgemental. It doesn't imply that a person's mind is deficient, but instead recognises
+    that their mind is performing as it has evolved to, and is trying to keep them safe. As such, much more self-compassion
+    often results from an exploration through the IFS lens, and self-compassion is a mediator of many distressing symptoms.
+    <br><br>
+    Additionally, operating under the assumption that the mind has parts can make it much easier to understand what is going on.
+    You can explore your mind, along with any internal conflict or contradictions,
+    without trying to present everything as consistent or logical.
+    <br><br>
+    <h3>Why question the gold standard?</h3>
+    Scientific investigation into therapy effectiveness is a soft science, and things are pretty crazy at the moment.
+    Psychology scientists thought that something called "ego depletion" was a real thing for almost 20 years, and thousands
+    of scientific studies supported its existence. However, within the last few years, more advanced statistical techniques
+    have revealed that it does not, in fact, exist - see
+    <a href="https://replicationindex.com/2016/04/18/is-replicability-report-ego-depletionreplicability-report-of-165-ego-depletion-articles/" target="_blank">here</a>
+    for a discussion. So, it's possible that something which doesn't exist can be shown to exist with a massive evidence base.
+    Further to this, 
+    <a href="resources/meta-analysis.pdf">here</a> is an article which provides evidence that psychic phenomena is real, using
+    the most stringent scientific methods we have invented.
+    <br><br>
+    How is this possible? Experimenter expectation has a huge effect on outcome.
+    <a href="resources/expectancy-effects.pdf">Here</a> is an article which looks at the effects of experimenter expectation
+    in a range of different domains, and found that it plays quite a significant effect.
+    <br><br>
+    CBT replaced psychoanalysis with the promise of being scientifically more valid, but
+    <a href="resources/cbt-analysis.pdf">here</a> is an article which demonstrates that psychoanalysis is not inferior
+    to CBT. Maybe that is just experimenter expectation at work again. But, maybe CBT has such a high volume of scientific evidence
+    because a therapy which involves challenging the logic of a person's thinking might appeal to the kinds of people who
+    end up in academia, and so it has been studied more prolifically, with many more opportunities for experimenter expectation
+    to demonstrate its efficacy.
+    <br><br>
+    Given the above, I think it's reasonable to question evidence-based ideas.
+    </div>
+    </div>
+    </div>`
+});
