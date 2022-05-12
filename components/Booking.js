895b8c4154ed8b59a1d83edf4554a8efa085d274
David O'Donohue <virtuspsychology@gmail.com>
2022-05-12 11:22:03 +1000

    commit

--- components/Booking.js
+++ components/Booking.js
@@ -1,6 +1,12 @@
 const Booking = Vue.component('booking', {
     template: `<div class="page-outer-frame">
     <div class="lander-filler"></div>
+    <div class="page-content">
+    <br><br>
+    Please note that payment is deducted upon booking an appointment. If you are eligible for a Medicare rebate, this can be processed
+    after the appointment has been attended, as long as a valid referral from a GP has been provided. 
+    <br><br>
+    </div>
     <iframe src="https://www.halaxy.com/book/widget/psychologist/mr-david-odonohue/724811/626281" class="booking-system"></iframe>
     </div>
     </div>`
