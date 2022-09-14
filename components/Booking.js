dbbbc19dfd6d1aff62a8a8d5c1a60233e9b72d64
David O'Donohue <virtuspsychology@gmail.com>
2022-09-14 16:52:07 +1000

    commit

--- components/Booking.js
+++ components/Booking.js
@@ -1,4 +1,17 @@
 const Booking = Vue.component('booking', {
+    data() {
+        return {
+            location: null
+        }
+    },
+    methods: {
+        setLocation(location){
+            this.location = location;
+        }
+    },
+    mounted(){
+        this.location = null;
+    },
     template: `<div class="page-outer-frame">
     <div class="lander-filler"></div>
     <div class="page-content">
@@ -7,7 +20,13 @@ const Booking = Vue.component('booking', {
     after the appointment has been attended, as long as a valid referral from a GP has been provided. 
     <br><br>
     </div>
-    <iframe src="https://www.halaxy.com/book/widget/psychologist/mr-david-odonohue/724811/626281" class="booking-system"></iframe>
+    <iframe v-if="location == 'OConnor'" src="https://www.halaxy.com/book/widget/psychologist/mr-david-odonohue/724811/626281" class="booking-system"></iframe>
+    <iframe v-else-if="location == 'Deakin'" src="https://www.halaxy.com/book/widget/psychologist/mr-david-odonohue/724811/837311" class="booking-system"></iframe>
+    <div v-else class="page-content">
+    <p>Which location would you like to make a booking for?</p><br><br>
+    <div @click="setLocation('OConnor')" class="button">O'Connor</div>
+    <div @click="setLocation('Deakin')" class="button">Deakin</div>
+    </div>
     </div>
     </div>`
 });
