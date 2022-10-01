2477402413eeaba4148a359e8e3ce01b542a9c2a
David O'Donohue <virtuspsychology@gmail.com>
2022-10-01 20:29:30 +1000

    commit

--- components/Vision.js
+++ components/Vision.js
@@ -19,8 +19,15 @@ const Vision = Vue.component('vision', {
     idea of not being attached? Should we criticise ourselves for being self-critical? Should we stop using "shoulds"? Is it good
     to stop judging things as "good" or "bad"? Are we really accepting the present moment if we are rejecting our resistance to it?
     Very confusing.<br><br>
+    <span class="quote">
+    "We cannot change anything unless we accept it."<br>
+    Carl Gustav Jung</span>
+    <br><br>
+    That certainly seems paradoxical.
+    <br><br>
     <span class="quote">"The words of truth are always paradoxical. To the mind that is still, the whole universe surrenders."<br>
-    Lao Tzu, Tao Te Ching (The Book of the Way and its Virtue)</span>
+    Lao Tzu, Tao Te Ching (The Book of the Way and its Virtue)
+    </span>
     </span>
     </div>
     </div>
