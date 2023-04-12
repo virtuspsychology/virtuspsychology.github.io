ea675ee5dfa128307e81c0964d8b7bef167f91bc
David O'Donohue <virtuspsychology@gmail.com>
2023-04-12 11:47:42 +1000

    commit

--- components/Booking.js
+++ components/Booking.js
@@ -19,6 +19,8 @@ const Booking = Vue.component('booking', {
     Please note that payment is deducted upon booking an appointment. If you are eligible for a Medicare rebate, this can be processed
     after the appointment has been attended, as long as a valid referral from a GP has been provided. 
     <br><br>
+    Please also note that I do not provide assessment services, and am not taking on new child clients (clients under the age of 18).
+    <br><br>
     </div>
     <iframe v-if="location == 'OConnor'" src="https://www.halaxy.com/book/widget/psychologist/mr-david-odonohue/724811/626281" class="booking-system"></iframe>
     <iframe v-else-if="location == 'Deakin'" src="https://www.halaxy.com/book/widget/psychologist/mr-david-odonohue/724811/837311" class="booking-system"></iframe>
